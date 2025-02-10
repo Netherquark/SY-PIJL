3db342abad861e96f6e3a321a6105af0d45db22a
Netherquark <netherquark@protonmail.com>
2025-02-10 10:57:52 +0530

    add documentation

--- Arrays_ArrayLists/ArrayListConversion.java
+++ Arrays_ArrayLists/ArrayListConversion.java
@@ -6,22 +6,33 @@
  */
 
  package Arrays_ArrayLists;
-
+ 
  import java.util.ArrayList;
  import java.util.Arrays;
  
  public class ArrayListConversion {
      public static void main(String[] args) {
+         // --- Array to ArrayList Conversion ---
+         // Use an Integer array (wrapper type) because generics do not work with primitives.
          Integer[] numArray = {1, 2, 3, 4, 5};
+         
+         // Convert array to ArrayList using Arrays.asList() and then create a modifiable ArrayList.
          ArrayList<Integer> arrayList = new ArrayList<>(Arrays.asList(numArray));
+         
+         // Display the ArrayList.
          System.out.println("Converted ArrayList from array: " + arrayList);
- 
+         
+         // --- ArrayList to Array Conversion ---
+         // Create an ArrayList with some String values.
          ArrayList<String> list = new ArrayList<>();
          list.add("Apple");
          list.add("Banana");
          list.add("Cherry");
          
+         // Convert the ArrayList to an array.
          String[] strArray = list.toArray(new String[list.size()]);
+         
+         // Display the array.
          System.out.print("Converted array from ArrayList: ");
          for (String s : strArray) {
              System.out.print(s + " ");
