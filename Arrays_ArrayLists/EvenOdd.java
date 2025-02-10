b2c13967684fce1852eb828494eb5419b9df4d9c
Netherquark <netherquark@protonmail.com>
2025-02-10 10:52:46 +0530

    add documentation

--- Arrays_ArrayLists/EvenOdd.java
+++ Arrays_ArrayLists/EvenOdd.java
@@ -8,21 +8,25 @@
  package Arrays_ArrayLists;
 
  import java.util.Scanner;
- 
+
  public class EvenOdd {
      public static void main(String[] args) {
          Scanner scanner = new Scanner(System.in);
          
+         // Prompt user to enter the number of elements.
          System.out.print("Enter the number of elements: ");
          int n = scanner.nextInt();
          
+         // Temporary array to store all input numbers.
          int[] numbers = new int[n];
          
+         // Accept the numbers from the user.
          System.out.println("Enter " + n + " integers:");
          for (int i = 0; i < n; i++) {
              numbers[i] = scanner.nextInt();
          }
          
+         // First pass: count even and odd numbers.
          int countEven = 0, countOdd = 0;
          for (int num : numbers) {
              if (num % 2 == 0) {
@@ -32,9 +36,11 @@
              }
          }
          
+         // Declare two arrays based on counts.
          int[] even = new int[countEven];
          int[] odd = new int[countOdd];
          
+         // Populate the even and odd arrays.
          int evenIndex = 0, oddIndex = 0;
          for (int num : numbers) {
              if (num % 2 == 0) {
@@ -44,12 +50,14 @@
              }
          }
          
+         // Display the even numbers.
          System.out.print("Even numbers: ");
          for (int e : even) {
              System.out.print(e + " ");
          }
          System.out.println();
          
+         // Display the odd numbers.
          System.out.print("Odd numbers: ");
          for (int o : odd) {
              System.out.print(o + " ");
